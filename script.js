b35e3f959761f0bf5bb641240c964ad44e73ec32
Finn André Breivik <finn.andre.breivik@gmail.com>
2020-05-13 19:47:23 +0200

    Uploaded file script.js for the Programming foundation course

---
+++ script.js
@@ -0,0 +1,29 @@
+// question 1
+var firstVar;
+
+// question 2
+var name = "Finn";
+
+// question 3
+var goals = 3;
+
+// question 4
+var division = 20 / 5;
+
+// question 5
+console.log(typeof "frog");
+
+// question 6
+var orderHasShipped = false;
+
+// question 7
+if (orderHasShipped === true) {
+	console.log("true");
+} else {
+	console.log("false");
+}
+
+// question 8
+for (i = 0; i <= 9; i++) {
+	console.log(i);
+}
